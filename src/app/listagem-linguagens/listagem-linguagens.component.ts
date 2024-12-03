a52535289732323a007a8747520b46e607fb0632
Helio Brito <helio.brito.bezerra@gmail.com>
2024-12-03 17:04:47 -0300

    Adequação para Angular 19

--- src/app/listagem-linguagens/listagem-linguagens.component.ts
+++ src/app/listagem-linguagens/listagem-linguagens.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-listagem-linguagens',
-    imports: [MatCardModule, MatToolbarModule, MatSlideToggleModule],
+    imports: [MatCardModule, MatToolbarModule],
     templateUrl: './listagem-linguagens.component.html',
     styleUrl: './listagem-linguagens.component.css'
 })
